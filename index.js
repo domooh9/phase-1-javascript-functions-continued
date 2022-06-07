e78de05815721596cb056d5138d6da829c74ff2e
Domnic Omondi <domooh9@gmail.com>
2022-06-07 13:41:21 +0300

    commit tree 44921c7a77de56489f5e55d7a22c9302b6391795

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+  }
+  
+  function mondayWork(activity='go to the office') {
+    return "This Monday, I will " + activity + ".";
+  }
+  
+  function wrapAdjective(adjective='*') {
+    const youAre = 'You are ';
+    return function(text) {
+      const closing = '!';
+      return youAre + adjective + text + adjective + closing;
+    }
+  }
